68dbc23793ae7a7da23a302a6d940d12df2b59fe
Tommy Parnell <tommy@terribledev.io>
2023-11-06 18:14:03 -0500

    ok it works

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "The Ramblings of TerribleDev",
 	url: "https://blog.terrible.dev/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "My name is Tommy Parnell. I usually go by TerribleDev on the internets. These are just some of my writings and rants about the software space.",
 	author: {
 		name: "Tommy Parnell",
 		url: "https://blog.terrible.dev/about/"
